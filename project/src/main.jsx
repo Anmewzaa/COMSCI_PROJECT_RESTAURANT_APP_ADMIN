7447745abc42e0299d48e70281d1957e88bb943d
Anmewzaa <punyakon857@gmail.com>
2024-07-01 21:51:55 +0700

    Update main.jsx

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -1,10 +1,32 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// CSS
 import "./global.css";
+// React Router Dom
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Pages
+import LoginPage from "./Admin/Pages/LoginPage";
+import HomePage from "./Admin/Pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "admin",
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
