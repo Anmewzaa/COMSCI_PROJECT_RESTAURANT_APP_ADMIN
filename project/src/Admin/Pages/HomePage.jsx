27fc7a280c8da4d61f615ed1978d52049e46a52f
Anmewzaa <punyakon857@gmail.com>
2024-07-04 17:01:15 +0700

    Update HomePage.jsx

--- project/src/Admin/Pages/HomePage.jsx
+++ project/src/Admin/Pages/HomePage.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+// React Hook
+import { useEffect } from "react";
+// React Router Dom
+import { Outlet } from "react-router-dom";
+// CSS
+import "../CSS/HomePage.css";
+// Componentes
+import NavBarComponent from "../Components/NavBarComponent";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  useEffect(() => {
+    if (!localStorage.getItem("PARADISE_LOGIN_TOKEN")) {
+      return (window.location.href = "/admin/login");
+    }
+  }, []);
+  return (
+    <>
+      <div className="admin-home-page">
+        <NavBarComponent />
+        <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
