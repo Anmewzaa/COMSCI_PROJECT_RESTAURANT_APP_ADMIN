1645a4f3026709ce861f7705e2e24a83e5189f6c
Anmewzaa <punyakon857@gmail.com>
2024-07-10 22:02:02 +0700

    Create MenuInfo.jsx

---
+++ project/src/Admin/Pages/MenuInfo.jsx
@@ -0,0 +1,42 @@
+// React Hook
+import { useState, useEffect } from "react";
+// React Router Dom
+import { useParams } from "react-router-dom";
+// axios
+import axios from "axios";
+
+const MenuInfo = () => {
+  const { id } = useParams();
+  const [menu, setMenu] = useState([]);
+  const [loading, setLoading] = useState();
+  const fetchAPI = async () => {
+    setLoading(true);
+    await axios
+      .get(`${import.meta.env.VITE_API_URL}/menu/get/${id}`)
+      .then((data) => {
+        setMenu(data.data.response);
+      });
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <>Loading...</>
+      ) : (
+        <>
+          <img
+            src={`${import.meta.env.VITE_API_URL}/images/${menu.menu_image}`}
+            alt=""
+          />
+          {menu && <>{menu?.menu_name?.thai}</>}
+        </>
+      )}
+    </>
+  );
+};
+
+export default MenuInfo;
