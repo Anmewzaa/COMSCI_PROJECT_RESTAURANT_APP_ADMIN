7c596a2a2dccbb6dc16b69a41e6dafa0d4ba0e48
Anmewzaa <punyakon857@gmail.com>
2024-07-10 22:02:00 +0700

    Update MenuPage.jsx

--- project/src/Admin/Pages/MenuPage.jsx
+++ project/src/Admin/Pages/MenuPage.jsx
@@ -2,19 +2,45 @@
 import { createContext, useState, useEffect } from "react";
 // Components
 import CategoriesComponent from "../Components/CategoriesComponent";
+import CardComponent from "../Components/CardComponent";
+import MenuFooter from "../Components/MenuFooter";
 // Context
-export const AppContext = createContext(null);
+export const SearchContext = createContext(null);
 // Axios
 import axios from "axios";
+// CSS
+import "../CSS/MenuPage.css";
 
 const MenuPage = () => {
   const [search, setSearch] = useState("");
-  const [menu, setMenu] = useState("");
+  const [menu, setMenu] = useState([]);
+  const [loading, setLoading] = useState();
+  const fetchAPI = async () => {
+    setLoading(true);
+    await axios.get(`${import.meta.env.VITE_API_URL}/menu/get`).then((data) => {
+      setMenu(data.data.response);
+    });
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
   return (
-    <AppContext.Provider value={{ search, setSearch }}>
+    <SearchContext.Provider value={{ search, setSearch }}>
       <CategoriesComponent />
-    </AppContext.Provider>
+      {loading ? (
+        <>Loading...</>
+      ) : (
+        <div className="card-box">
+          {menu &&
+            menu.map((item) => {
+              return <CardComponent key={item.menu_id} menu={item} />;
+            })}
+        </div>
+      )}
+      <MenuFooter />
+    </SearchContext.Provider>
   );
 };
 
