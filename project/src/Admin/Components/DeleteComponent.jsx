2b703e86150852d4dbe6f10282b8a5d3cc54c839
Anmewzaa <punyakon857@gmail.com>
2024-07-12 15:52:03 +0700

    Create DeleteComponent.jsx

---
+++ project/src/Admin/Components/DeleteComponent.jsx
@@ -0,0 +1,45 @@
+// Axios
+import axios from "axios";
+// SWAL
+import Swal from "sweetalert2";
+
+// eslint-disable-next-line react/prop-types
+const DeleteComponent = ({ name, id }) => {
+  const formSubmit = () => {
+    Swal.fire({
+      title: "ต้องการลบใช่ไหม?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const JWT_TOKEN = localStorage.getItem("PARADISE_LOGIN_TOKEN");
+        axios
+          .delete(`${import.meta.env.VITE_API_URL}/${name}/delete/${id}`, {
+            headers: {
+              Authorization: `Bearer ${JWT_TOKEN}`,
+            },
+          })
+          .then(() => {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+            window.location.replace("/admin/menu/categories");
+          });
+      }
+    });
+  };
+
+  return (
+    <>
+      <button onClick={formSubmit}>ลบ</button>
+    </>
+  );
+};
+
+export default DeleteComponent;
