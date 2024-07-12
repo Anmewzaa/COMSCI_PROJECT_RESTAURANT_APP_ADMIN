c33087365c690d0f514d96db142615e5e5c49a16
Anmewzaa <punyakon857@gmail.com>
2024-07-12 15:51:53 +0700

    Update CategoriesPage.jsx

--- project/src/Admin/Pages/CategoriesPage.jsx
+++ project/src/Admin/Pages/CategoriesPage.jsx
@@ -2,10 +2,23 @@
 import axios from "axios";
 // React Hook
 import { useState, useEffect } from "react";
+// Components
+import BackFooter from "../Components/BackFooter";
+// React Router Dom
+import { Link } from "react-router-dom";
+// Components
+import EditComponent from "../Components/EditComponent";
+import DeleteComponent from "../Components/DeleteComponent";
 
 const CategoriesPage = () => {
   const [categories, setCategories] = useState([]);
-  const fetchAPI = () => {};
+  const fetchAPI = async () => {
+    await axios
+      .get(`${import.meta.env.VITE_API_URL}/category/get`)
+      .then((data) => {
+        setCategories(data.data.response);
+      });
+  };
   useEffect(() => {
     fetchAPI();
   }, []);
@@ -13,25 +26,39 @@ const CategoriesPage = () => {
     <>
       <div>
         <input type="text" placeholder="ค้นหาหมวดหมู่อาหาร" />
-        <button>เพิ่มหมวดหมู่</button>
+        <Link to={"create"}>เพิ่มหมวดหมู่</Link>
       </div>
       <table>
         <tr>
           <th>ชื่อ</th>
-          <th>Create date</th>
+          <th>Create Date</th>
+          <th>Update Date</th>
           <th>Action</th>
         </tr>
-        <tr>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-        </tr>
+        {categories &&
+          categories.map((item) => {
+            const formattedCreateDate = new Date(
+              item?.createdAt
+            ).toLocaleString();
+            const formattedLastUpdateDate = new Date(
+              item?.updatedAt
+            ).toLocaleString();
+            return (
+              <>
+                <tr key={item?.category_id}>
+                  <td>{`${item?.category_name?.thai} (${item?.category_name?.english})`}</td>
+                  <td>{formattedCreateDate}</td>
+                  <td>{formattedLastUpdateDate}</td>
+                  <td>
+                    <EditComponent id={item?.category_id} />
+                    <DeleteComponent id={item?.category_id} name={"category"} />
+                  </td>
+                </tr>
+              </>
+            );
+          })}
       </table>
+      <BackFooter props={"/admin/menu"} />
     </>
   );
 };
