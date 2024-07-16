b925517e348a55afe35fe21777e7271467a04055
Anmewzaa <punyakon857@gmail.com>
2024-07-16 18:05:23 +0700

    Create EditCategoriesPage.jsx

---
+++ project/src/Admin/Pages/CategoryPage/EditCategoriesPage.jsx
@@ -0,0 +1,96 @@
+// Axios
+import axios from "axios";
+// React Hook
+import { useState, useEffect } from "react";
+// React Router Dom
+import { useParams } from "react-router-dom";
+// SWAL
+import Swal from "sweetalert2";
+
+const EditCategoriesPage = () => {
+  const { id } = useParams();
+  const [categories, setCategories] = useState({
+    thai: "",
+    english: "",
+  });
+  const fetchAPI = async () => {
+    await axios
+      .get(`${import.meta.env.VITE_API_URL}/category/get/${id}`)
+      .then((data) => {
+        setInitialValue("thai", data.data.response.category_name.thai);
+        setInitialValue("english", data.data.response.category_name.english);
+      });
+  };
+  const setInitialValue = (name, value) => {
+    setCategories((prevCategories) => ({
+      ...prevCategories,
+      [name]: value,
+    }));
+  };
+  const inputValue = (name) => (event) => {
+    setCategories({ ...categories, [name]: event.target.value });
+  };
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+  const submitForm = async (e) => {
+    e.preventDefault();
+    if (!(categories.thai && categories.english)) {
+      return Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "กรุณากรอกให้ครบถ้วน",
+      });
+    }
+    const JWT_TOKEN = await localStorage.getItem("PARADISE_LOGIN_TOKEN");
+    await axios
+      .put(
+        `${import.meta.env.VITE_API_URL}/category/update/${id}`,
+        {
+          category_name_thai: categories.thai,
+          category_name_english: categories.english,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${JWT_TOKEN}`,
+          },
+        }
+      )
+      .then((result) => {
+        if (result.data.error) {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: result.data.error,
+          });
+        } else {
+          Swal.fire({
+            icon: "success",
+            title: "อัพเดทรายการอาหารเสร็จสมบูรณ์",
+            text: "...",
+          }).then(() => {
+            window.location.href = "/admin/menu/categories";
+          });
+        }
+      });
+  };
+  return (
+    <>
+      <form onSubmit={submitForm}>
+        <input
+          type="text"
+          value={categories.thai}
+          onChange={inputValue("thai")}
+        />
+        <input
+          type="text"
+          value={categories.english}
+          onChange={inputValue("english")}
+        />
+        <button type="submit">อัพเดทหมวดหมู่อาหาร</button>
+      </form>
+    </>
+  );
+};
+
+export default EditCategoriesPage;
