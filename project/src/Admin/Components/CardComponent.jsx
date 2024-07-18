44e53b8f8fa20f9bf80240465fba040d8bdbb492
Anmewzaa <punyakon857@gmail.com>
2024-07-18 17:10:48 +0700

    Update CardComponent.jsx

--- project/src/Admin/Components/CardComponent.jsx
+++ project/src/Admin/Components/CardComponent.jsx
@@ -1,19 +1,22 @@
 // React Router Dom
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // CSS
 import "../CSS/CardComponent.css";
 
 /* eslint-disable react/prop-types */
 const CardComponent = ({ menu }) => {
+  const navigate = useNavigate();
   return (
-    <div className="card">
+    <div className="card" onClick={() => navigate(`${menu.menu_id}`)}>
       <img
         src={`${import.meta.env.VITE_API_URL}/images/${menu.menu_image}`}
         alt=""
       />
       <h2>{menu.menu_name.thai}</h2>
-      <p>{menu.menu_price}</p>
-      <Link to={`${menu.menu_id}`}>Info</Link>
+      <div className="container">
+        <p>{menu.menu_price} บาท</p>
+        <button>แจ้งเมนูหมด</button>
+      </div>
     </div>
   );
 };
