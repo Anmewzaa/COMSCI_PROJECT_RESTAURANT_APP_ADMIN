b3a9c1eb1c099bd8cd670d9ca00ac179774db7f9
Anmewzaa <punyakon857@gmail.com>
2024-07-18 17:10:56 +0700

    Update MenuInfo.jsx

--- project/src/Admin/Pages/MenuPage/MenuInfo.jsx
+++ project/src/Admin/Pages/MenuPage/MenuInfo.jsx
@@ -6,6 +6,8 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 // Components
 import BackFooter from "../../Components/BackFooter";
+// SWAL
+import Swal from "sweetalert2";
 
 const MenuInfo = () => {
   const { id } = useParams();
@@ -23,6 +25,36 @@ const MenuInfo = () => {
   useEffect(() => {
     fetchAPI();
   }, []);
+  const editMenuInfo = () => {};
+  const deleteMenuInfo = () => {
+    Swal.fire({
+      title: "ต้องการลบใช่ไหม?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const JWT_TOKEN = localStorage.getItem("PARADISE_LOGIN_TOKEN");
+        axios
+          .delete(`${import.meta.env.VITE_API_URL}/menu/delete/${id}`, {
+            headers: {
+              Authorization: `Bearer ${JWT_TOKEN}`,
+            },
+          })
+          .then(() => {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+            window.location.replace(`/admin/menu`);
+          });
+      }
+    });
+  };
 
   return (
     <>
@@ -31,15 +63,49 @@ const MenuInfo = () => {
       ) : (
         <>
           {menu ? (
-            <>
-              <img
-                src={`${import.meta.env.VITE_API_URL}/images/${
-                  menu.menu_image
-                }`}
-                alt=""
-              />
-              {menu?.menu_name?.thai}
-            </>
+            <div className="menu-info-box">
+              <>
+                <img
+                  src={`${import.meta.env.VITE_API_URL}/images/${
+                    menu.menu_image
+                  }`}
+                  alt="menu images"
+                  className="menu-info-image"
+                />
+              </>
+              <div className="menu-infomation">
+                <h2>
+                  {menu?.menu_name?.thai} ({menu?.menu_name?.english})
+                </h2>
+                <p>
+                  <span>ราคา</span> {menu?.menu_price} บาท
+                </p>
+                <p>
+                  <span>คำอธิบาย</span>
+                  <br />
+                  <p>
+                    {menu?.menu_describe?.thai} ({menu?.menu_describe?.english})
+                  </p>
+                </p>
+                <span>ส่วนเสริม</span>
+                {menu.menu_option_id ? (
+                  <>
+                    {menu.menu_option_id &&
+                      menu.menu_option_id.map((item) => {
+                        return (
+                          <div key={item._id}>
+                            {item.option_name.thai} ({item.option_name.english})
+                          </div>
+                        );
+                      })}
+                  </>
+                ) : (
+                  <>Empty</>
+                )}
+                <button onClick={() => editMenuInfo()}>แก้ไข</button>
+                <button onClick={() => deleteMenuInfo()}>ลบ</button>
+              </div>
+            </div>
           ) : (
             <>Empty</>
           )}
