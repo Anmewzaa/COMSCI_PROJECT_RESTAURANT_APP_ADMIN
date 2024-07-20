bc89e63809d7c513a7cffbd278fc74c51be26d40
Anmewzaa <punyakon857@gmail.com>
2024-07-20 17:10:02 +0700

    Update EditOptionPage.jsx

--- project/src/Admin/Pages/OptionPage/EditOptionPage.jsx
+++ project/src/Admin/Pages/OptionPage/EditOptionPage.jsx
@@ -6,10 +6,12 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 // SWAL
 import Swal from "sweetalert2";
+// Components
+import BackFooter from "../../Components/BackFooter";
 
 const EditOptionPage = () => {
   const { id } = useParams();
-  const [options, setOptions] = useState({
+  const [option, setOption] = useState({
     thai: "",
     english: "",
     sub_option: [],
@@ -28,63 +30,27 @@ const EditOptionPage = () => {
       });
   };
   const setInitialValue = (name, value) => {
-    setOptions((prevOptions) => ({
+    setOption((prevOptions) => ({
       ...prevOptions,
       [name]: value,
     }));
   };
-  const addSubOption = async () => {
-    if (!(subOption.thai && subOption.english)) {
-      return;
-    }
-    setOptions((prevOption) => ({
-      ...prevOption,
-      sub_option: [
-        ...prevOption.sub_option,
-        {
-          sub_option_name: {
-            thai: subOption.thai,
-            english: subOption.english,
-          },
-        },
-      ],
-    }));
-    setSubOption({ ...subOption, ["thai"]: "", ["english"]: "" });
-  };
   const inputValue = (name) => (event) => {
-    setOptions({ ...options, [name]: event.target.value });
+    setOption({ ...option, [name]: event.target.value });
   };
   const inputSubOptionValue = (name) => (event) => {
     setSubOption({ ...subOption, [name]: event.target.value });
   };
-  const deleteSubOption = async (indexToRemove) => {
-    setOptions((prevOption) => ({
-      ...prevOption,
-      sub_option: prevOption.sub_option.filter(
-        (_, index) => index !== indexToRemove
-      ),
-    }));
-  };
-  useEffect(() => {
-    fetchAPI();
-  }, []);
   const submitForm = async (e) => {
     e.preventDefault();
-    if (!(options.thai && options.english)) {
-      return Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "กรุณากรอกให้ครบถ้วน",
-      });
-    }
     const JWT_TOKEN = await localStorage.getItem("PARADISE_LOGIN_TOKEN");
     await axios
       .put(
         `${import.meta.env.VITE_API_URL}/option/update/${id}`,
         {
-          option_name_thai: options.thai,
-          option_name_english: options.english,
-          sub_option: options.sub_option,
+          option_name_thai: option.thai,
+          option_name_english: option.english,
+          sub_option: option.sub_option,
         },
         {
           headers: {
@@ -110,56 +76,127 @@ const EditOptionPage = () => {
         }
       });
   };
+  const addSubOption = async () => {
+    if (!(subOption.thai && subOption.english)) {
+      return;
+    }
+    setOption((prevOption) => ({
+      ...prevOption,
+      sub_option: [
+        ...prevOption.sub_option,
+        {
+          sub_option_name: {
+            thai: subOption.thai,
+            english: subOption.english,
+          },
+        },
+      ],
+    }));
+    setSubOption({ ...subOption, ["thai"]: "", ["english"]: "" });
+  };
+  const deleteSubOption = async (indexToRemove) => {
+    setOption((prevOption) => ({
+      ...prevOption,
+      sub_option: prevOption.sub_option.filter(
+        (_, index) => index !== indexToRemove
+      ),
+    }));
+  };
+  useEffect(() => {
+    fetchAPI();
+  }, []);
   return (
     <>
-      <form onSubmit={submitForm}>
-        <input type="text" value={options.thai} onChange={inputValue("thai")} />
-        <input
-          type="text"
-          value={options.english}
-          onChange={inputValue("english")}
-        />
-        <div>
-          <input
-            type="text"
-            value={subOption.thai}
-            placeholder="sub option thai"
-            onChange={inputSubOptionValue("thai")}
-          />
-          <input
-            type="text"
-            value={subOption.english}
-            placeholder="sub option english"
-            onChange={inputSubOptionValue("english")}
-          />
-          <button type="button" onClick={() => addSubOption()}>
-            Add
-          </button>
+      <form onSubmit={submitForm} className="form">
+        <div className="form-menu-container">
+          <div>
+            <label>ชื่อส่วนเสริมอาหารภาษาไทย</label>
+            <input
+              type="text"
+              placeholder="ชื่อส่วนเสริมอาหารภาษาไทย"
+              value={option.thai}
+              onChange={inputValue("thai")}
+              required
+            />
+          </div>
+          <div>
+            <label>ชื่อส่วนเสริมอาหารภาษาอังกฤษ</label>
+            <input
+              type="text"
+              placeholder="ชื่อส่วนเสริมอาหารภาษาอังกฤษ"
+              value={option.english}
+              onChange={inputValue("english")}
+              required
+            />
+          </div>
         </div>
-        {options.sub_option ? (
-          <>
-            {options.sub_option.map((item, index) => {
-              return (
-                <div key={index}>
-                  {`${item.sub_option_name.thai} (${item.sub_option_name.english})`}
-                  <button
-                    type="button"
-                    onClick={() => {
-                      deleteSubOption(index);
-                    }}
-                  >
-                    x
-                  </button>
-                </div>
-              );
-            })}
-          </>
-        ) : (
-          <>Empty Sub Option</>
-        )}
-        <button type="submit">อัพเดทส่วนเสริมอาหาร</button>
+        <div className="mb-1">
+          <div className="form-menu-container">
+            <div>
+              <label>ชื่อส่วนเสริมอาหารย่อยภาษาไทย</label>
+              <input
+                type="text"
+                value={subOption.thai}
+                placeholder="ชื่อส่วนเสริมอาหารย่อยภาษาไทย"
+                onChange={inputSubOptionValue("thai")}
+              />
+            </div>
+            <div>
+              <label>ชื่อส่วนเสริมอาหารย่อยภาษาอังกฤษ</label>
+              <input
+                type="text"
+                value={subOption.english}
+                placeholder="ชื่อส่วนเสริมอาหารย่อยภาษาอังกฤษ"
+                onChange={inputSubOptionValue("english")}
+              />
+            </div>
+          </div>
+          <button
+            type="button"
+            onClick={() => addSubOption()}
+            className="btn-add-sub"
+          >
+            เพิ่มรายการย่อย
+          </button>
+          <table className="sub-box">
+            <thead>
+              <tr>
+                <th>ชื่อรายการย่อยภาษาไทย</th>
+                <th>ชื่อรายการย่อยภาษาอังกฤษ</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {option.sub_option &&
+                option.sub_option.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>
+                        <>{item.sub_option_name.thai}</>
+                      </td>
+                      <td>{item.sub_option_name.english}</td>
+                      <td>
+                        <button
+                          type="button"
+                          onClick={() => {
+                            deleteSubOption(index);
+                          }}
+                          className="btn btn-red"
+                        >
+                          ลบรายการย่อย
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+        <button type="submit" className="btn-add-sub">
+          อัพเดทเสริมอาหาร
+        </button>
       </form>
-      {JSON.stringify(options)}
+      <BackFooter props={"/admin/menu/option"} />
     </>
   );
 };
