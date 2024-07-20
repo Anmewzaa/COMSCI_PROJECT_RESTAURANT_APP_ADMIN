9668abde04f6222a326ec0fc1c2f4ef7609dd39a
Anmewzaa <punyakon857@gmail.com>
2024-07-20 17:10:27 +0700

    Update MenuFooter.jsx

--- project/src/Admin/Components/MenuFooter.jsx
+++ project/src/Admin/Components/MenuFooter.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const MenuFooter = () => {
   return (
     <>
-      <ul className="footer-box">
+      <ul className="footer-box flex-end">
         <li className="item">
           <Link to={"categories"}>หมวดหมู่อาหาร</Link>
         </li>
