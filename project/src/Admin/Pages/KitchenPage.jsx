ce88085941695a590d46a57776919424a675cd66
Anmewzaa <punyakon857@gmail.com>
2024-07-30 10:05:44 +0700

    Update KitchenPage.jsx

--- project/src/Admin/Pages/KitchenPage.jsx
+++ project/src/Admin/Pages/KitchenPage.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+// React Hook
+import { useState, useEffect } from "react";
+// Axios
+import axios from "axios";
 
 const KitchenPage = () => {
-  return <div>KitchenPage</div>;
+  const [menu_1, setMenu_1] = useState([]);
+  const fetchAPI = async () => {
+    await axios
+      .get(`${import.meta.env.VITE_API_URL}/menu/get/${id}`)
+      .then((data) => {
+        const filteredMenu1 = data.filter((item) => item.status === 1);
+        const filteredMenu2 = data.filter((item) => item.status === 2);
+        const filteredMenu3 = data.filter((item) => item.status === 3);
+        setMenu_1(filteredMenu1);
+      });
+  };
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+  return (
+    <>
+      <div>menu 1{JSON.stringify(menu_1)}</div>
+    </>
+  );
 };
 
 export default KitchenPage;
