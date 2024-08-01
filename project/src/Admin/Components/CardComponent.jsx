97c452f2d13de1ae776f33bfa24361afc43b500e
Anmewzaa <punyakon857@gmail.com>
2024-08-01 23:09:59 +0700

    Update CardComponent.jsx

--- project/src/Admin/Components/CardComponent.jsx
+++ project/src/Admin/Components/CardComponent.jsx
@@ -7,16 +7,24 @@ import "../CSS/CardComponent.css";
 const CardComponent = ({ menu }) => {
   const navigate = useNavigate();
   return (
-    <div className="card cursor" onClick={() => navigate(`${menu.menu_id}`)}>
-      <img
-        src={`${import.meta.env.VITE_API_URL}/images/${menu.menu_image}`}
-        alt=""
-      />
-      <h2>{menu.menu_name.thai}</h2>
-      <div className="container">
-        <p>{menu.menu_price} บาท</p>
+    <>
+      <div
+        className="card cursor relative"
+        onClick={() => navigate(`${menu.menu_id}`)}
+      >
+        <div>
+          <img
+            src={`${import.meta.env.VITE_API_URL}/images/${menu.menu_image}`}
+            alt=""
+          />
+          <h2 className="sarabun-semibold">{menu.menu_name.thai}</h2>
+          <p className="sarabun-regular">{menu.menu_price} บาท</p>
+        </div>
+        {menu.menu_status === false && (
+          <div className="card-inactive sarabun-semibold">สินค้าหมด</div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
